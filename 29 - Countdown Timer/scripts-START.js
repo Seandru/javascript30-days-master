9876cc3976c8e844b88754b5fe0bb0f0cf0ff9f9
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-22 13:54:16 +0300

    add timer display on page and button functionality

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,22 +1,57 @@
+//variables declaration
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
-function timer(seconds){
-    const now = Date.now();
-    const then = now + seconds * 1000;
-    displayTimeLeft(seconds)
+//functions
+function timer(seconds) {
+  clearInterval(countdown); //clear existing timers
 
-    countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
-        //check if we need to stop it
-        if (secondsLeft < 0){
-            clearInterval(countdown);
-            return;
-        }
-        //display
-        displayTimeLeft(seconds)
-    }, 1000);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if(secondsLeft < 0) { // check if we should stop it
+      clearInterval(countdown);
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
 }
 
-function displayTimeLeft(seconds){
-    console.log(seconds);
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  // ${hour > 12 ? hour - 12 : hour} is used to convert 24h format to 12h format
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+//events
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
